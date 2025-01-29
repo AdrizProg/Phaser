747498cff00378beab76e199194c48754c4c5520
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2025-01-29 18:12:18 +0000

    Subiendo el codigo

---
+++ src/scenes/Game.js
@@ -0,0 +1,33 @@
+import { Scene } from 'phaser';
+
+export class Game extends Scene {
+    constructor() {
+        super('Game');
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor(0x00ff00);
+
+        this.add.image(512, 384, 'background').setAlpha(0.5);
+
+        // this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+        //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        //     stroke: '#000000', strokeThickness: 8,
+        //     align: 'center'
+        // }).setOrigin(0.5);
+
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('GameOver');
+
+        });
+
+        // platforms = this.physics.add.staticGroup();
+
+        // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        // platforms.create(600, 400, 'ground');
+        // platforms.create(50, 250, 'ground');
+        // platforms.create(750, 220, 'ground');
+    }
+}
---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,38 @@
+import { Scene } from 'phaser';
+
+export class MainMenu extends Scene
+{
+    constructor ()
+    {
+        super('MainMenu');
+    }
+
+    create ()
+    {
+        this.add.image(512, 384, 'background');
+
+        const logo = this.add.image(512, 300, 'logo');
+
+        this.add.text(512, 460, 'Bienvenido', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#000',
+            stroke: '#fff', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.tweens.add({
+            targets: logo,
+            y: 250,
+            duration: 1000, 
+
+            ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo:true
+        });
+
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('Game');
+
+        });
+    }
+}
